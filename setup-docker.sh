f4b791ba421715e1bdbc68ad22d5582c916a0cd0
Daniil Gorodilov <dealenx@gmail.com>
2021-02-17 20:35:31 +0700

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -4,6 +4,6 @@ cp env.docker.php env.php
 docker-compose up -d
 docker-compose exec webserver composer install &&  chmod 777 .
 
-cp -r ./cms/wp-content/themes/* ./wp-content/themes
-
 sudo chmod 777 -R ./wp-content/
+
+cp -r ./cms/wp-content/themes/* ./wp-content/themes
